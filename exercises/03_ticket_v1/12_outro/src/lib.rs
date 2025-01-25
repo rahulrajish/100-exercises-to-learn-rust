a131269b0514da55d66b99f713d472e5202b8d6c
Rahul Rajish Nathan <74354544+rahulrajish@users.noreply.github.com>
2025-01-25 16:57:42 +0530

    Completed Excercises
    3.06
    3.07
    3.08
    3.09
    3.10
    3.11
    3.12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,82 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::product_name_is_empty(&product_name);
+        Self::product_name_greater_than_300(&product_name);
+        Self::quantity_eq_to_zero(quantity);
+        Self::unit_price_eq_to_zero(unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name:String) {
+        Self::product_name_is_empty(&product_name);
+        Self::product_name_greater_than_300(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Self::quantity_eq_to_zero(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Self::unit_price_eq_to_zero(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    fn product_name_is_empty(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product Name cannot be empty!");
+        }
+        
+    }
+
+    fn product_name_greater_than_300(product_name: &String) {
+        if product_name.len() > 300 {
+            panic!("Product Name cannot be more than 300 characters!");
+        }
+    }
+
+    fn quantity_eq_to_zero(quantity: u32) {
+        if quantity == 0 {
+            panic!("Quantity should be greater than zero!");
+        }
+    }
+
+    fn unit_price_eq_to_zero(unit_price: u32) {
+        if unit_price == 0 {
+            panic!("Unit price should be greater than zero");
+        }
+    }
+
+}
